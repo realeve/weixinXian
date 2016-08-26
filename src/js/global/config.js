664fc85cfd82aa3b2b2b80b8412d58b0bb3c2a47
realeve@qq.com <lb159753>
2016-08-26 14:43:50 +0800

    release v1.3

--- src/js/global/config.js
+++ src/js/global/config.js
@@ -1,5 +1,15 @@
 ﻿var SPORT = require('./sport.js');
 
+function getUrlParam(name) {
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+	var r = encodeURI(window.location.search).substr(1).match(reg); //匹配目标参数
+	if (r !== null) return decodeURI(r[2]);
+	return null; //返回参数值
+}
+
+var answerNum = getUrlParam('num');
+num = (num == null) ? 50 : num;
+
 var exam = {
 	loadComplete: false,
 	total: 0, //总分
@@ -8,16 +18,16 @@ var exam = {
 	isAnswered: [], //题目回答状态
 	timeReleased: false, //时间用尽
 	isStarted: false, //活动是否开始
-	timeLength: 20*60*1000, //启用时间限制 0为不限制
+	timeLength: 20 * 60 * 1000, //启用时间限制 0为不限制
 	sourceList: [], //原题目顺序
 	scoresPerAnswer: 0, //每道题目分数
 	isSubmit: false, //数据是否提交
 	isLogin: false, //是否登录
 	loginData: {}, //用户登录信息
-	maxAnswerNum: 100, //最大抽取多少道题目
+	maxAnswerNum: num, //最大抽取多少道题目
 	answerTimes: 0, //每个用户最多回答几次
 	examPaper: "cost", //试卷文件
-	sportDate: "8月30日",
+	sportDate: "9月1日",
 	sportid: SPORT.XIAN,
 	editAnswer: false, //允许修改答案
 	curID: 0, //当前答题数
