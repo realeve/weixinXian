92a006755a20426429002e5fa13eb9b3b84ebf44
realeve <realeve@qq.com>
2016-11-27 14:51:36 +0800

    数说修改

--- src/js/global/paper.js
+++ src/js/global/paper.js
@@ -1,2 +1,2 @@
-var PAPER = ['safe', 'party3', 'party3', 'cost', 'costgw', 'party5', 'party3'];
+var PAPER = ['safe', 'party3', 'party3', 'cost', 'costgw', 'party5', 'xian'];
 module.exports = PAPER;
